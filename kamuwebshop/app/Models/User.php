78f9c72177d9d332ee85efdac9af2e8fdecdf9e5
MoSzabina <moszabina@gmail.com>
2026-08-22 12:51:32 +0200

    modify_default_usermodel

--- kamuwebshop/app/Models/User.php
+++ kamuwebshop/app/Models/User.php
@@ -2,62 +2,51 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Carbon;
 
 /**
  * @property int $id
- * @property string $name
+ * @property string $username
  * @property string $email
- * @property Carbon|null $email_verified_at
  * @property string $password
- * @property string|null $two_factor_secret
- * @property string|null $two_factor_recovery_codes
- * @property Carbon|null $two_factor_confirmed_at
+ * @property string|null $shipping
+ * @property bool $is_admin
  * @property string|null $remember_token
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  */
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
+#[Fillable(['username', 'email', 'password', 'shipping', 'is_admin'])]
+#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
         ];
     }
 
     public function orders()
-{
-    return $this->hasMany(Order::class);
-}
+    {
+        return $this->hasMany(Order::class);
+    }
 
-public function cart()
-{
-    return $this->hasOne(Cart::class);
-}
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
 
-public function creditTransactions()
-{
-    return $this->hasMany(CreditTransaction::class);
-}
-
-
+    public function creditTransactions()
+    {
+        return $this->hasMany(CreditTransaction::class);
+    }
 }
