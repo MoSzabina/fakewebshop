c571c2b30a06fbdf9f2fdb01a20b6e265837a860
MoSzabina <moszabina@gmail.com>
2026-09-02 14:17:17 +0200

    add_slug_to_products

--- kamuwebshop/database/migrations/2026_08_20_075242_create_products_table.php
+++ kamuwebshop/database/migrations/2026_08_20_075242_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained('categories');
             $table->string('name');
+            $table->string('slug')->unique();
             $table->decimal('price');
             $table->integer('stock');
             $table->string('description');
