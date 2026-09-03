76b82481905dbcbc1cd342bbd0d5226f2d95f84c
MoSzabina <moszabina@gmail.com>
2026-09-03 18:25:20 +0200

    refactor_simplify_auth

--- kamuwebshop/app/Http/Controllers/Settings/SecurityController.php
+++ kamuwebshop/app/Http/Controllers/Settings/SecurityController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Settings;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Settings\PasswordUpdateRequest;
-use App\Http\Requests\Settings\TwoFactorAuthenticationRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Validation\Rules\Password;
 use Inertia\Inertia;
@@ -12,28 +11,17 @@ use Inertia\Response;
 
 class SecurityController extends Controller
 {
-    /**
-     * Show the user's security settings page.
-     */
-    public function edit(TwoFactorAuthenticationRequest $request): Response
-    {
-        $props = [
-            'passwordRules' => Password::defaults()->toPasswordRulesString(),
-        ];
-
-        return Inertia::render('settings/security', $props);
-    }
-
-    /**
-     * Update the user's password.
-     */
+    
     public function update(PasswordUpdateRequest $request): RedirectResponse
     {
         $request->user()->update([
             'password' => $request->password,
         ]);
 
-        Inertia::flash('toast', ['type' => 'success', 'message' => __('Password updated.')]);
+        Inertia::flash('toast', [
+            'type' => 'success',
+            'message' => __('Jelszó frissítve'),
+        ]);
 
         return back();
     }
